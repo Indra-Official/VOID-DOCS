47450ada22ae8d6ae4bd753305f3ff1929b2939a
Indra <indra.rtf.official@gmail.com>
2025-10-29 17:44:13 +0530

    killing jk child

--- MobileApp/Main.py
+++ MobileApp/Main.py
@@ -6,7 +6,6 @@ from dontcommit import MongoDB
 app = Flask(__name__)
 x = MongoDB
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
--- raw_code(ignore)/raw.js
+++
@@ -1,5 +0,0 @@
-function side_slide(){
-    document.getElementById("how").addEventListener("click", function(){this.textContent = ">>"})
-    document.getElementById("side").classList.toggle("active")
-    
-}
